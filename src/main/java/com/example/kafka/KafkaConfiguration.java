b09a17fefce3e67ca0b0faae28cf87c89d9b153d
Manish Kumar <manish.jangid08@gmail.com>
2021-04-25 20:28:34 -0700

    Update KafkaConfiguration.java

--- src/main/java/com/example/kafka/KafkaConfiguration.java
+++ src/main/java/com/example/kafka/KafkaConfiguration.java
@@ -27,7 +27,7 @@ public class KafkaConfiguration {
     public ProducerFactory<String, Student> producerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-bootstrap:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
