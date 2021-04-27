0f131cbf7e6554c2310374dbfb72d6600a4111a0
Manish Kumar <manish.jangid08@gmail.com>
2021-04-26 19:43:48 -0700

    Create RestfulConfiguration.java

---
+++ src/main/java/com/example/kafka/RestfulConfiguration.java
@@ -0,0 +1,29 @@
+/*
+ * Defining the @Bean configuration to set the CORS configuration support globally 
+ * for the Spring Boot application
+ */
+
+package com.example.kafka;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+@Configuration
+public class RestfulConfiguration {
+    @Bean
+    public CorsFilter crossOriginFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE"); 
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
