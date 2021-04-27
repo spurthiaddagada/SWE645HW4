2b4ecc77de163f795d5147d66baa58bc1e2d1881
Manish Kumar <manish.jangid08@gmail.com>
2021-04-26 18:41:36 -0700

    Update KafkaApplication.java

--- src/main/java/com/example/kafka/KafkaApplication.java
+++ src/main/java/com/example/kafka/KafkaApplication.java
@@ -14,7 +14,7 @@ public class KafkaApplication {
 	}
 	@Bean
    public WebMvcConfigurer corsConfigurer() {
-       return new WebMvcConfigurerAdapter() {
+       return new WebMvcConfigurer() {
            @Override
            public void addCorsMappings(CorsRegistry registry) {
                registry.addMapping("/**").allowedOrigins("*");
