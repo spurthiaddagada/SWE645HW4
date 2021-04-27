855c3cfb93610b0b5605a5d95d6941fd056ba382
spurthiaddagada <67293229+spurthiaddagada@users.noreply.github.com>
2021-04-26 20:33:13 -0500

    Update KafkaApplication.java

--- src/main/java/com/example/kafka/KafkaApplication.java
+++ src/main/java/com/example/kafka/KafkaApplication.java
@@ -9,5 +9,14 @@ public class KafkaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaApplication.class, args);
 	}
+	@Bean
+   public WebMvcConfigurer corsConfigurer() {
+       return new WebMvcConfigurerAdapter() {
+           @Override
+           public void addCorsMappings(CorsRegistry registry) {
+               registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+           }
+       };
+   }
 
 }
