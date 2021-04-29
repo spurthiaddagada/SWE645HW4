f384dfb9f26e96d02fe8453852769ece77d94894
spurthiaddagada <67293229+spurthiaddagada@users.noreply.github.com>
2021-04-29 17:10:33 -0500

    Update StudentResource.java
    added import statements

--- src/main/java/com/example/kafka/StudentResource.java
+++ src/main/java/com/example/kafka/StudentResource.java
@@ -22,7 +22,8 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
